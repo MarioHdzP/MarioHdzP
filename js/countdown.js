9f161fa6f5ac6a254d04c91c94d043e684aa5e74
MarioHdzP <107303206+MarioHdzP@users.noreply.github.com>
2022-06-13 17:10:27 -0500

    prueba cuenta regresiva

--- js/countdown.js
+++ js/countdown.js
@@ -1,9 +1,9 @@
 simplyCountdown('#cuenta', {
 	year: 2022, // required
-	month: 11, // required
-	day: 18, // required
-	hours: 18, // Default is 0 [0-23] integer
-	minutes: 0, // Default is 0 [0-59] integer
+	month: 06, // required
+	day: 13, // required
+	hours: 12, // Default is 0 [0-23] integer
+	minutes: 12, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
